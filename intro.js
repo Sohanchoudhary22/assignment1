0d078056b60aaa926c57551accd318407b67ea8f
Sohan <er.skchoudhary22@gmail.com>
2024-03-21 16:55:00 +0530

    update

---
+++ intro.js
@@ -0,0 +1,37 @@
+// Q.1:- write a program to gretest three numbers using ternary operator.
+
+// let a=22 ;
+// let b=25 ;
+// let c=12;
+// a>b && a>c ? console.log("a is greater") : a<b && b>c ? console.log("b is greater") : console.log("c is greater")
+
+//Result - b is greater
+
+
+
+// Q.2:- write a program positive and negative Number using ternary operator.
+
+// let a=-2;
+// a>=0 ? console.log("a is positive") :console.log("a is negative")
+
+//Result - a is negative
+
+
+
+// Q.3:- Imagine you have one variable is logged=true. Write a Javascript statement that assigns the string "Welcome back!" to a variable greeting if is loggedIn is true,
+//      otherwise assigns "Please log in.". Utilize a ternary operator to achieve this. 
+
+// let loggedIn = true
+// loggedIn == true ? console.log("Welcome back!") : console.log("Please log in")
+
+//Result - Welcome back!
+
+
+
+// Q.4:-   Suppose you have one variable dayOfweek="Monday" representing the day of the Week. Write a javascript statement that assigns "not Weekend" to a variable
+//         if dayOfweek is "Saturday" or "Sunday", otherwise assigns "Weekend". Use if else condition to solve this Program.
+
+let dayOfweek = "Monday";
+dayOfweek =="Saturday"|| dayOfweek =="Sunday" ? console.log("not Weekend") : console.log("Weekend")
+
+Result - Weekend
